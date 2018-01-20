799ba773fed8b04d8792533c3e900b71d9091464
Diamondlord <vladimir.almazov@gmail.com>
2018-01-20 15:31:23 +0200

    undestanding firebase

--- app.js
+++ app.js
@@ -182,7 +182,7 @@ function getToken() {
 
 
 function sendNotification(notification) {
-    var key = 'AAAAaGQ_q2M:APA91bGCEOduj8HM6gP24w2LEnesqM2zkL_qx2PJUSBjjeGSdJhCrDoJf_WbT7wpQZrynHlESAoZ1VHX9Nro6W_tqpJ3Aw-A292SVe_4Ho7tJQCQxSezDCoJsnqXjoaouMYIwr34vZTs';
+    var key = 'AAAAJyW0l_Q:APA91bHLP16NQNvy-57HrmeGrTnZGIUsXA_yjRSzsW7geQNvcE695ib_ewaOJlErDwrZnaq62kvQS8kxsiLLfKqoxn5B1eUWVsDCg7yuDiX-WP1xGOglTZoYgLkBR10uYNHcp7UNeBaL';
 
     console.log('Send notification', notification);
 
